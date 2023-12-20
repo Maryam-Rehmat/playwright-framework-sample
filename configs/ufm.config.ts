a3137b96b2a6f828ce4b27bb2d87334931122f2c
Santhoshkumar G <santhoshkumar.govindasamy@walmart.com>
2023-12-20 17:01:16 +0530

    test

--- configs/ufm.config.ts
+++ configs/ufm.config.ts
@@ -20,8 +20,8 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ?[["@walmartlabs/testburst-playwright-js-reporter"],["html"]]:"html",
-  //reporter: [["@walmartlabs/testburst-playwright-js-reporter"]],
+  //reporter: process.env.CI ?[["@walmartlabs/testburst-playwright-js-reporter"],["html"]]:"html",
+  reporter: [["@walmartlabs/testburst-playwright-js-reporter"]],
   //reporter: 'allure-playwright',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
