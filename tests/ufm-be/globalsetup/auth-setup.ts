dfd7d5585ba338958058f2ff4cab045d93ca0a5c
Santhoshkumar G <santhoshkumar.govindasamy@walmart.com>
2023-12-20 18:42:49 +0530

    test

--- tests/ufm-be/globalsetup/auth-setup.ts
+++ tests/ufm-be/globalsetup/auth-setup.ts
@@ -3,5 +3,6 @@ import { Utility } from '../../common/fixtures/utils';
 
 
 setup('authenticate', async ({ request }) => {
+    console.log("testburst app : ",process.env.appName);
     process.env.token = await Utility.getTokenForUser('s0g0fhn',request);
 });
