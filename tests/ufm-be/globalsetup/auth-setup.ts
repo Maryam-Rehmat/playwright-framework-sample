09f334d2363a177f5633b0ba8be3b8c6e7ea3366
Lokesh Patni - l0p07dx <Lokesh.Patni@walmart.com>
2023-12-21 11:26:15 +0530

    Update auth-setup.ts

--- tests/ufm-be/globalsetup/auth-setup.ts
+++ tests/ufm-be/globalsetup/auth-setup.ts
@@ -1,7 +1,6 @@
 import { test as setup, expect } from '@playwright/test';
 import { Utility } from '../../common/fixtures/utils';
 
-
 setup('authenticate', async ({ request }) => {
     process.env.token = await Utility.getTokenForUser('s0g0fhn',request);
 });
