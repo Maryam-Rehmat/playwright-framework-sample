6925adfa3ac4de6c33621b9a2a31742dbd7af299
Richa <richa.garg.garg@walmart.com>
2023-12-29 15:49:46 +0530

    baselinefinalcomments

--- tests/ufm-be/testcases/baseline-api.spec.ts
+++ tests/ufm-be/testcases/baseline-api.spec.ts
@@ -6,6 +6,7 @@ import debug from "debug";
 test.describe("Validating the baseline forecast Details @Regresion @Smoke @baseline", () => 
 {
   let apiResponse,apiResponseBody,forcastNames;
+  // API endpoint URL
   const testdata = testdatafile.configEndpoint;
   const endpointURL = `${process.env.apiBaseURLUFM}${testdata.endpoint}`;
   debug.log(endpointURL);  
@@ -14,11 +15,13 @@ test.describe("Validating the baseline forecast Details @Regresion @Smoke @basel
     //get request for the baseline details includes Primary forecast
     await test.step("Make a Get Request for baseline Details including Primary forecast", async () => {
       debug.log("Make a Get Request for baseline Details including Primary forecast");
+       //API Request
       const apiResponse = await context.request.get(endpointURL,{params: {
                 "dim_id": testdata.sba_datas.queryParams.dim_id,
                 "classification_id": testdata.sba_datas.queryParams.classification_id,
                 "dim_val_id":testdata.sba_datas.queryParams.dim_val_id,
                 "include_primary":testdata.sba_datas.queryParams.include_primary}});
+      //Extracting the response body     
       apiResponseBody = await apiResponse.json();
       debug.log('API response:',apiResponseBody);
 
@@ -29,23 +32,24 @@ test.describe("Validating the baseline forecast Details @Regresion @Smoke @basel
                 //identify the baseline forecast names using the key 'name' from the list of object array
                 forcastNames = apiResponseBody.row_data.map((obj: any) => obj.name);
                 debug.log('Forecast names are : ' + forcastNames)
-                //Using ForeEach loop iterating all the forecasts available in baseline forecast list
-                forcastNames.forEach((forecast: string) => {
-                //Validating the expected sba name contains in Primary forecast
-                    expect(forcastNames).toContain(testdata.forecast_name);  
+                // checking that the result contains primary forecast
+                expect(forcastNames).toContain(testdata.forecast_name);  
                     debug.log('sba name contains Primary forecast');
-                });
+               
   });
 });
 test("Checking the baseline Details excluding Primary forecast ", async ({ context }) => {
     //get request for the baseline details excluding Primary forecast
     await test.step("Make a Get Request for baseline Details excluding Primary forecaste", async () => {
       debug.log("Make a Get Request for baseline Details excluding Primary forecast");
+       //API Request
       const apiResponse = await context.request.get(endpointURL,{params: {
                 "dim_id": testdata.sba_datas.queryParams.dim_id,
                 "classification_id": testdata.sba_datas.queryParams.classification_id,
                 "dim_val_id":testdata.sba_datas.queryParams.dim_val_id,
-                "include_primary":testdata.sba_datas.include_primary}});
+                "include_primary":testdata.sba_datas.exclude_primary}});
+
+      //Extracting the response body
       apiResponseBody = await apiResponse.json();
       debug.log('API response:',apiResponseBody);
     });
@@ -54,12 +58,10 @@ test("Checking the baseline Details excluding Primary forecast ", async ({ conte
                 //identify the baseline forecast names using the key 'name' from the list of object array
                 forcastNames = apiResponseBody.row_data.map((obj: any) => obj.name);
                 debug.log('Forecast names are : ' + forcastNames)
-                //Using ForeEach loop iterating all the forecasts available in Baseline forecast list
-                forcastNames.forEach((forecast: string) => {
-                //Validating the expected sba name contains in Primary forecast
+                // checking that the result does not contain primary forecast
                 expect(forcastNames).not.toContain(testdata.forecast_name);  
                 debug.log('sba name does not contain Primary forecast');
-                });
+                
   });
 });
 });
