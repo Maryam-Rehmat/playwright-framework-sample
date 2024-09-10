c780c452533adaeb030fc771499954b0fe2b6845
Maryam-Rehmat <134375279+Maryam-Rehmat@users.noreply.github.com>
2024-09-10 16:45:19 +0500

    Update authentication-setup.ts

--- tests/portfolio-fe/setup/authentication-setup.ts
+++ tests/portfolio-fe/setup/authentication-setup.ts
@@ -2,5 +2,5 @@ import { test as setup, expect } from '../../common/fixtures/test-hook'
 import { Utility } from '../../common/fixtures/utils';
 
 setup('authenticate for portfolio users', async ({ request }) => {
-    process.env.token = await Utility.getTokenForUser('vn501a8', request);
+    process.env.token = await Utility.getTokenForUser('vn501aa8', request);
 });
