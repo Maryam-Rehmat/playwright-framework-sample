36d500a555eceaa20d5f7b18dbd7f6eedd9ffd40
Maryam-Rehmat <134375279+Maryam-Rehmat@users.noreply.github.com>
2024-09-10 16:46:16 +0500

    Update auth-setup.ts

--- tests/ufm-be/globalsetup/auth-setup.ts
+++ tests/ufm-be/globalsetup/auth-setup.ts
@@ -2,5 +2,5 @@ import { test as setup, expect } from '../../common/fixtures/test-hook'
 import { Utility } from '../../common/fixtures/utils';
 
 setup('authenticate', async ({ request }) => {
-    process.env.token = await Utility.getTokenForUser('s0g0fhn', request);
+    process.env.token = await Utility.getTokenForUser('dummy', request);
 });
